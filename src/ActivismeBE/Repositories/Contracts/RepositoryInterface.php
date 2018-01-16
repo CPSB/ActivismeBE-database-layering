3f25fcc7f753611a7b526f2ce869ad21076dbce9
Tim Joosten <c1039614@student.thomasmore.be>
2018-01-16 09:42:49 +0100

    Implement type hints repository interface

--- src/ActivismeBE/Repositories/Contracts/RepositoryInterface.php
+++ src/ActivismeBE/Repositories/Contracts/RepositoryInterface.php
@@ -14,21 +14,19 @@ interface RepositoryInterface
     /**
      * Get all the rows for the database table.
      *
-     * @param array $columns The columns u want to use in your view.
-     *
+     * @param  array $columns The columns u want to use in your view.
      * @return mixed
      */
-    public function all($columns = ['*']);
+    public function all(array $columns = ['*']);
 
     /**
      * Paginate the database table results.
      *
-     * @param integer $perPage The ata rows per page in the view.
-     * @param array   $columns The coolumns u want to use in your view.
-     *
+     * @param  int     $perPage The ata rows per page in the view.
+     * @param  array   $columns The coolumns u want to use in your view.
      * @return mixed
      */
-    public function paginate($perPage = 1, $columns = ['*']);
+    public function paginate(int $perPage = 1, array $columns = ['*']);
 
     /**
      * Create a new row in the database table.
@@ -42,90 +40,81 @@ interface RepositoryInterface
     /**
      * Save a model without mass assignment
      *
-     * @param array $data The data u want to store in the database.
-     *
+     * @param  array $data The data u want to store in the database.
      * @return bool
      */
-    public function saveModel(array $data);
+    public function saveModel(array $data): bool;
 
     /**
      * Update a record in the database table.
      *
-     * @param array   $data         The input data fields u want to update in the db.
-     * @param integer $primaryKey   The PK in the database.
-     *
+     * @param  array   $data         The input data fields u want to update in the db.
+     * @param  int     $primaryKey   The PK in the database.
      * @return mixed
      */
-    public function update(array $data, $primaryKey);
+    public function update(array $data, int $primaryKey);
 
     /**
      * Delete a record in the database.
      *
-     * @param integer $primaryKey The resource id in the database. (PK)
-     *
+     * @param  int $primaryKey The resource id in the database. (PK)
      * @return mixed
      */
-    public function delete($primaryKey);
+    public function delete(int $primaryKey);
 
     /**
      * Delete all by field and value.
      *
-     * @param string $field     The database column name.
-     * @param string $selector  The where clause selector.
-     * @param string $value     The value for the given database column.
-     *
+     * @param  string $field     The database column name.
+     * @param  string $selector  The where clause selector.
+     * @param  string $value     The value for the given database column.
      * @return boolean
      */
-    public function deleteAllBy($field, $selector, $value);
+    public function deleteAllBy(string $field, string $selector, string $value): bool;
 
     /**
      * Find a record in the database based on the primary key.
      *
-     * @param integer $primaryKey       The resource id in the database. (PK)
-     * @param array   $columns          The db table columns that u want to use in your view.
-     *
+     * @param  int     $primaryKey       The resource id in the database. (PK)
+     * @param  array   $columns          The db table columns that u want to use in your view.
      * @return mixed
      */
-    public function find($primaryKey, $columns = ['*']);
+    public function find(int $primaryKey, array $columns = ['*']);
 
     /**
      * Try to find a record in the database based on the primary key.
      *
-     * @param integer $primaryKey The primary key in the database table.
-     *
+     * @param  int   $primaryKey The primary key in the database table.
      * @return void
      */
-    public function findOrFail($primaryKey);
+    public function findOrFail(int $primaryKey);
 
     /**
      * Find a collection of models by the given query conditions.
      *
-     * @param string $field   The field where u want to search on.
-     * @param string $value   The value in the above given field.
-     * @param array  $columns The database columns u want to use on the view.
-     *
+     * @param  string $field   The field where u want to search on.
+     * @param  string $value   The value in the above given field.
+     * @param  array  $columns The database columns u want to use on the view.
      * @return mixed
      */
-    public function findBy($field, $value, $columns = ['*']);
+    public function findBy(string $field, string $value, array $columns = ['*']);
 
     /**
      * Find a database record by value and column.
      *
-     * @param string $field   The database column name.
-     * @param string $value   The value in the database column.
-     * @param array  $columns The database table columns u want to use in your view.
-     *
+     * @param  string $field   The database column name.
+     * @param  string $value   The value in the database column.
+     * @param  array  $columns The database table columns u want to use in your view.
      * @return mixed
      */
-    public function findAllBy($field, $value, $columns = ['*']);
+    public function findAllBy(string $field, string $value, array $columns = ['*']);
 
     /**
      * Find a collection of models by the given query conditions.
      *
-     * @param array $where   The keys and values where u want to search on.
-     * @param array $columns The database columns u want to use in the view.
-     *
+     * @param  array $where   The keys and values where u want to search on.
+     * @param  array $columns The database columns u want to use in the view.
      * @return mixed
      */
-    public function findWhere($where, $columns = ['*']);
+    public function findWhere(array $where, array $columns = ['*']);
 }
